551373842b9561fa56a10f2a9067da748260fac8
Jobaer Chowdhury <jobaer.du@gmail.com>
2015-03-14 23:07:37 +0600

    Delete AesonTest.hs

--- src/AesonTest.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import Data.Aeson ((.:), (.:?), decode, FromJSON(..), Value(..))
-import Control.Applicative ((<$>), (<*>))
-import qualified Data.ByteString.Lazy.Char8 as BS
-
